b80894c9aeb440916893b6e366b0a4d5515476d4
tramhuuducvn <tramhuuduc@gmail.com>
2023-04-20 17:37:14 +0700

    saved

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import styles from "./App.module.css";
+import Products from "./Products";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className={styles.App}>
+      <div className={styles.container}>
+        <div className={styles.wrap}>
+          <div className={styles.box}>
+            <Products />
+          </div>
+          <div className={styles.box}>
+            <Products />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Products/index.tsx
+++ src/Products/index.tsx
@@ -1,23 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
+import styles from "./styles.module.css";
+import nike_logo from "../assets/nike.png";
 
 function Products() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.root}>
+      <div className={styles.container}>
+        <div className={styles.headerIcon}>
+          <img src={nike_logo} alt="Nike Icon" width="100" height="52" />
+        </div>
+        <div className={styles.headerIcon}>
+          <p className={styles.title}>Out Products</p>
+        </div>
+
+        <div className={styles.list}>
+          <div className={styles.product}></div>
+          <div className={styles.product}></div>
+          <div className={styles.product}></div>
+          <div className={styles.product}></div>
+          <div className={styles.product}></div>
+          <div className={styles.product}></div>
+          <div className={styles.product}></div>
+          <div className={styles.product}></div>
+          <div className={styles.product}></div>
+          <div className={styles.product}></div>
+          <div className={styles.product}></div>
+          <div className={styles.product}></div>
+        </div>
+      </div>
     </div>
   );
 }
